5725c9334c52d8999a49cbd4647cfda64fc7fe3e
pooja <152240500+pooja-71414@users.noreply.github.com>
2023-12-28 14:20:31 +0530

    Create data_type.cs

---
+++ data_type.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b,c;
+            a=12;
+            b=20;
+            c=30;
+            // methode  no 1-----
+            Console.WriteLine("value of a ="+a+"  value of b ="+b+"  value of c ="+ c);
+            // methode  no 2-----
+            Console.WriteLine("value of a ={0}  value of b ={1}  value of c ={2}",a,b,c);
+            Console.Read();
+        }
+    }
+}
