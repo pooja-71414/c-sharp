27a0f4494398bc38a2372a5deaa8e896968b0829
pooja <152240500+pooja-71414@users.noreply.github.com>
2023-12-28 14:19:48 +0530

    Create use_console_method.cs

---
+++ use_console_method.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("writeline use c# program");
+            Console.Write("write use c# program");
+            Console.ReadLine();
+            Console.Read();
+        }
+    }
+}
