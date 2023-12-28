fb499f77b6054e28b555094cb32c1b87ff6e2607
pooja <152240500+pooja-71414@users.noreply.github.com>
2023-12-28 14:23:49 +0530

    Create type_casting(implicit).cs

---
+++ type_casting(implicit).cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace dream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       
+            int i = 10;
+            float f;
+            f = i;
+            Console.WriteLine("value of int i = {0}",i);
+            Console.WriteLine("int value converse into float type that means f = i therefore f = {0}",f);
+            Console.Read();
+        }
+    }
+}
