0064a79c38a2954c93b73d611e13c6303e677f51
pooja <152240500+pooja-71414@users.noreply.github.com>
2024-01-01 11:39:29 +0530

    Create indexer.cs

---
+++ indexer.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programs
+{
+    internal class Program
+    {
+        class A
+        {
+            int size = 10;
+            int i;
+            string[] m=new string[10];
+            public A()
+            {
+                for (i = 0; i < size; i++)
+                {
+                    m[i] = "india";
+                    //Console.WriteLine(m[i]);
+                }
+            }
+            public string this[int i] 
+            { 
+                get { return m[i]; }
+                set { m[i] = value; }       
+            }
+        }
+        static void Main(string[] args)
+        {
+            //use of indexer---
+            A s = new A();
+            s[2] = "korea";
+            s[4] = "hindustan";
+            s[7] = "gujarat";
+            s[9] = "bharat";
+            for (int i = 0;i<10;i++)
+            {
+                Console.WriteLine(s[i]);
+            }
+
+                          
+          Console.Read();
+        }
+    }
+}
